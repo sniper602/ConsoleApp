a1814bd440bfe2cb6b820998761f51a44d42a1e2
Zhang <zhang.dengke.dick@vertivco.com>
2019-11-18 14:33:49 +0800

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1/DBAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class DBAccess
+    {
+        public static void ExecuteNoQuery()
+        {
+
+            Console.WriteLine("测试专用");
+
+        }
+    }
+}
